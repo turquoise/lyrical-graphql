68f9f7e37f6125e6d706647081d8ea88dce40b67
M Gosschalk <monica.gosschalk@btinternet.com>
2017-11-28 00:36:32 +0000

    added likes

---
+++ client/components/LyricList.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
+
+class LyricList extends Component {
+
+  onLike(id, likes) {
+    //console.log(id);
+    this.props.mutate({
+      variables: { id: id},
+      optimisticResponse: {
+        __typename: 'Mutation',
+        likeLyric: {
+          id: id,
+          __typename: 'LyricType',
+          likes: likes + 1
+        }
+      }
+    });
+
+  }
+
+  renderLyrics() {
+    return this.props.lyrics.map( (lyric, i) => {
+      return (
+        <li key={i} className="collection-item" >
+          {lyric.content}
+          <div className="vote-box">
+            <i className="material-icons" onClick={ () => this.onLike(lyric.id, lyric.likes)}>
+              thumb_up
+            </i>
+            {lyric.likes}
+          </div>
+        </li>
+      );
+    });
+  }
+
+  render() {
+    return (
+      <ul className="collection">
+        {this.renderLyrics()}
+      </ul>
+    );
+  }
+}
+
+const mutation = gql`
+  mutation LikeLyric($id: ID) {
+    likeLyric(id: $id) {
+      id
+      likes
+    }
+  }
+`;
+
+export default graphql(mutation)(LyricList);
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://admin:password@ds027749.mlab.com:27749/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
